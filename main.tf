a11453c9906c8c2cab732dabfea6fa05d95c52f6
Moises <mdelgado@example.com>
2023-03-16 14:45:57 -0400

    Updated main.tf with AWS autoscaling group

--- main.tf
+++ main.tf
@@ -1,19 +1,32 @@
-resource "aws_instance" "helloec2" {
-    ami                     = "ami-0172070f66a8ebe63"
+resource "aws_launch_configuration" "launchconfig_example" {
+    image_id                = "ami-0172070f66a8ebe63"
     instance_type           = "t2.micro"
-    vpc_security_group_ids  = [aws_security_group.hello_sg.id]
+    security_groups         = [aws_security_group.hello_sg.id]
 
 
     user_data = <<-EOF
                 #!/bin/bash
                 echo "Hello world!" > index.html
-                nohup busybox httpd -f -p 8080 &
+                nohup busybox httpd -f -p ${var.server_port} &
                 EOF
 
-    user_data_replace_on_change = true
+    # Required when using a launch configuration with an auto scaling group.
+    lifecycle {
+        create_before_destroy = true
+    }
+}
 
-    tags = {
-        Name = "terraform-example"
+resource "aws_autoscaling_group" "asg_example" {
+    launch_configuration    = aws_launch_configuration.launchconfig_example.name
+    vpc_zone_identifier     = data.aws_subnets.default.ids
+
+    min_size = 2
+    max_size = 10
+
+    tag {
+        key                 = "Name"
+        value               = "terraform-asg-example"
+        propagate_at_launch = true
     }
 }
 
@@ -21,9 +34,25 @@ resource "aws_security_group" "hello_sg" {
     name = "terraform-helloworld-secgroup"
 
     ingress {
-        from_port   = 8080
-        to_port     = 8080
+        from_port   = var.server_port
+        to_port     = var.server_port
         protocol    = "tcp"
         cidr_blocks =["0.0.0.0/0"]
     }
+}
+
+data "aws_vpc" "default" {
+    default = true
+}
+
+data "aws_subnets" "default" {
+    filter {
+        name        = "vpc-id"
+        values      = [data.aws_vpc.default.id]
+    }
+}
+
+variable "server_port" {
+    description = "The port number the web server will use for HTTP requests"
+    type        = number
 }
